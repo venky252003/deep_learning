0cfd3c702094324b30d05a6812026c84e27f2086
venky252003 <venky25@gmail.com>
2021-10-02 22:51:31 +0530

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # deep_learning
 Deep Learning Course
+
+
+## Setup Conda
+conda create -n NNCode python=3.8 -y
+
+## Activate Conda
+conda activate NNCode
+
+## Deactivate Conda
+conda deactivate
+
+## Check List of Conda
+conda list
+
+## Install all Packages
+pip install -r requirements.txt
---
+++ add.py
@@ -0,0 +1,41 @@
+from utils.all_utils import *
+from utils.model import Perceptron
+
+AND = {
+    "x1": [0,0,1,1],
+    "x2": [0,1,0,1],
+    "y": [0,0,0,1],
+}
+
+ETA = 0.3 # 0 and 1
+EPOCHS = 10
+model = Perceptron(eta=ETA, epochs=EPOCHS)
+
+def create_dataset():
+    df = pd.DataFrame(AND)
+    X,y = prepare_data(df)
+    return X,y
+
+def create_model(X, y):    
+    model.fit(X, y)
+    _ = model.total_loss()
+       
+
+def model_predict(X):
+    model.predict(X)
+
+def load_model(path):
+    model = joblib.load(path)
+    return model    
+
+def predict_load_model(inputs, path):
+    model = load_model(path)
+    model.predict(inputs)
+    
+
+if __name__ == '__main__':
+    model_path = "model//and.model"
+    X, y = create_dataset()
+    create_model(X, y)
+    model_predict(X)
+    save_model(model, model_path)
